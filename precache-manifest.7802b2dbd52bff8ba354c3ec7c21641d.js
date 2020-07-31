950384f5119f9ce8c056929b37463a638f37fa22
Niranjan420 <niranjanrajesh02@gmail.com>
2020-07-31 18:25:01 +0530

    Updates

---
+++ precache-manifest.7802b2dbd52bff8ba354c3ec7c21641d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5461a49e92c5726457a41c0e28bf7c94",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "245c2305ffd68a18a3ce",
+    "url": "/corona-tracker/static/css/main.f6b1236c.chunk.css"
+  },
+  {
+    "revision": "b43aaa888ed22f65fa04",
+    "url": "/corona-tracker/static/js/2.cf5fde01.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.cf5fde01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "245c2305ffd68a18a3ce",
+    "url": "/corona-tracker/static/js/main.20336f18.chunk.js"
+  },
+  {
+    "revision": "9c4d7b1cf1de7ae99f79",
+    "url": "/corona-tracker/static/js/runtime-main.bf840863.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
