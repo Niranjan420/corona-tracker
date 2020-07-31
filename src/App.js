2d9871cb28ef212b02cd779961dccae66d7320d0
Niranjan420 <niranjanrajesh02@gmail.com>
2020-07-31 17:28:13 +0530

    Graph 1.0 + responsive css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
 import { fetchData } from "./api";
+import coronaImg from "./images/image.png";
 
 export default class App extends Component {
-  state = { data: {} };
+  state = { data: {}, country: "" };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
@@ -17,15 +18,17 @@ export default class App extends Component {
     const fetchedData = await fetchData(country);
     console.log(country);
     console.log(fetchedData);
+    this.setState({ data: fetchedData, country: country });
   };
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
+        <img className={styles.img} src={coronaImg} alt="Corona Img" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
